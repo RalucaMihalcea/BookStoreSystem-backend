373ad86a4b7e7e734e3ead41205db9eb2e5125b2
Raluca <Raluca_mih97@yahoo.com>
2017-11-03 18:17:02 +0200

    partea de login realizata si cea de register

--- src/main/java/com/example/bookstoresystembackend/repository/UserRepository.java
+++ src/main/java/com/example/bookstoresystembackend/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package com.example.bookstoresystembackend.repository;
 
 import com.example.bookstoresystembackend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
+    User findUserByUsernameAndPassword(String username, String password);
     User findByUsernameAndPassword(String username, String password);
-    User findByUsername(String username);
 
 }
